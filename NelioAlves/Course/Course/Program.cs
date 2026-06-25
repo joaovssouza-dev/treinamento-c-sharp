6d63cd9a6d77cc1da1ba0b8ddc10cda6c33046d4
João Vitor S. Souza <jv.santiago.souza@gmail.com>
2026-06-25 18:29:47 -0300

    conversões implícitas e casting

--- NelioAlves/Course/Course/Program.cs
+++ NelioAlves/Course/Course/Program.cs
@@ -4,44 +4,17 @@
     {
         static void Main(string[] args)
         {
-            bool completo = false;
+            float a = 4.5f;
+            double b = a; // converte implicitamente
+            double c = 4.5;
+            float d = (float)c; // não consegue converter implicitamente, somente com o casting
+            int e = (int)c; // mesmo problema de cima
 
-            char genero = 'F';
-            char letra = '\u0041';
+            var f = 5;
+            var g = 2;
 
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 2147483648L;
-            float n5 = 4.5f;
-            double n6 = 4.5;
-
-            string nome = "Maria Green";
-
-            object obj1 = "Alex Brown";
-            object obj2 = 4.5f;
-
-            Console.WriteLine(completo);
-
-            Console.WriteLine(genero);
-            Console.WriteLine(letra);
-
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(n5);
-            Console.WriteLine(n6);
-
-            Console.WriteLine(nome);
-
-            //int a = int.MinValue;
-            //int b = int.MaxValue;
-            //sbyte c = sbyte.MinValue;
-            //sbyte d = sbyte.MaxValue;
-            //decimal e = decimal.MaxValue;
-
-            Console.WriteLine();
+            double resultado = (double) f / g; // precisa de casting, senão retorna valor com mesmo tipo das variáveis
+            Console.WriteLine(resultado);
         }
     }
 }
